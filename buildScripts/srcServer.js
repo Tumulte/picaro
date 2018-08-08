4703d0bac9d2579a346d39c148e0512bfbda1ddd
Tumulte <etienne@audiofanzine.net>
2018-08-09 01:00:51 +0200

    test

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,7 +5,6 @@
 //- créer un type de quote «temoignage» (d'autres)
 // +lier des quotes entre elles autour de ? (idée ? concepte ???)
 
-console.log("connected to port : " + port);
 
 var express = require('express');
 //Tools
