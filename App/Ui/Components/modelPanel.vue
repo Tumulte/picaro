2c28c12dca5a9819d90dd1401942b8bfa4fa2cd5
Étienne <etienne@audiofanzine.net>
2020-02-04 11:24:20 +0100

    wip

---
+++ App/Ui/Components/modelPanel.vue
@@ -0,0 +1,77 @@
+<script>
+    const template = require("./modelPanel.pug").default;
+
+    const panelComponent = {
+        template: template,
+        /**
+         * @type {function}
+         */
+        data() {
+            return {
+                dataType: [
+                    {
+                        name: "text",
+                        component: "v-text-field"
+                    },
+                    {
+                        name: "boolean",
+                        component: "v-switch"
+                    },
+                    {
+                        name: "number",
+                        component: "v-text-field"
+                    },
+                    {
+                        name: "range",
+                        component: "v-text-field"
+                    },
+                    {
+                        name: "date",
+                        component: "v-text-field"
+                    },
+                    {
+                        name: "tags",
+                        component: "v-text-field"
+                    },
+                    {
+                        name: "relation",
+                        component: "v-text-field"
+                    },
+                    {
+                        name: "radio",
+                        component: "v-text-field"
+                    },
+                    {
+                        name: "select",
+                        component: "v-text-field"
+                    }
+                ],
+                modelCollection: [],
+                selectedModelIndex: null,
+                selectedType: null,
+                typeCollection: []
+            };
+        },
+        methods: {
+            addModel() {
+                this.modelCollection.push({
+                    name: "model" + this.modelCollection.length
+                });
+                this.selectedModelIndex = this.modelCollection.length - 1;
+            },
+            selectModel(index) {
+                this.selectedModelIndex = index;
+            },
+            addType() {
+                this.typeCollection.push(this.selectedType);
+            }
+        },
+        computed: {
+            model() {
+                return this.modelCollection[this.selectedModelIndex];
+            }
+        }
+    };
+
+    module.exports = panelComponent;
+</script>
